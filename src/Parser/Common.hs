7f30e4ea3484d628ef0d693a45269e8fa4c641a1
alecsferra <aferrarini01@gmail.com>
2019-12-07 21:03:21 +0100

    integer

--- src/Parser/Common.hs
+++ src/Parser/Common.hs
@@ -1,24 +1,25 @@
 module Parser.Common where
 
 import Parser.Parser
-import Data.Char (isSpace, isDigit)
-import Control.Applicative (many, (<|>))
+import Data.Char
+import Control.Applicative
 import Debug.Trace (trace)
+import GHC.IO.Unsafe (unsafePerformIO)
 
-parseChar :: Char -> Parser Char
-parseChar expected = Parser parse
+char :: Char -> Parser Char
+char expected = Parser parse
   where
     parse ((line, char), found:rest)
       | found == expected = Right (pos,          rest, found)
       | otherwise =         Left  ((line, char), [[expected]])
-        where pos = if expected == '\n' then (line + 1, 0) else (line, char + 1)
+        where pos = if found == '\n' then (line + 1, 0) else (line, char + 1)
     parse ((line, char), []) = Left ((line, char), [[expected]])
 
-parseString :: String -> Parser String
-parseString = traverse parseChar
+string :: String -> Parser String
+string = traverse char
 
-parseMatch :: (Char -> Bool) -> Parser String
-parseMatch f =
+while :: (Char -> Bool) -> Parser String
+while f =
   Parser $ \((line, char), val) ->
     let (parsed, tokens') = span f val
         newLines = length $ filter ('\n' ==) parsed
@@ -26,24 +27,40 @@ parseMatch f =
         pos = if newLines > 0 then (newLines, char') else (line, char + char')
      in Right (pos, tokens', parsed)
 
+match :: (Char -> Bool) -> String -> Parser Char
+match condition message = Parser parse
+  where
+    parse ((line, char), found:rest)
+      | condition found = Right (pos,          rest, found)
+      | otherwise =         Left  ((line, char), [message])
+        where pos = if found == '\n' then (line + 1, 0) else (line, char + 1)
+    parse ((line, char), []) = Left ((line, char), [message])
 
 whitespace :: Parser String
-whitespace = parseMatch isSpace
+whitespace = while isSpace
 
 stringLiteral :: Parser String
-stringLiteral = parseChar '\"' *> parseMatch ('\"' /=) <* parseChar '\"'
+stringLiteral = char '\"' *> while ('\"' /=) <* char '\"'
 
 separatedBy :: Parser a -> Parser b -> Parser [a]
 separatedBy element separator = (:) <$> element <*> many (separator *> element) <|> pure []
 
--- BROKEN TODO: Fix
-integer :: Parser Integer
-integer =
-  Parser $ \(pos, tokens) -> do
-    (pos', tokens', sign)     <- run signParser (pos, tokens)
-    (pos'', tokens'', digits) <- run (parseMatch isDigit) (pos', tokens')
-    Right (pos'' `debug` (sign:digits) , tokens'', read (sign:digits))
-    where
-      signParser = parseChar '+' <|> parseChar '-' <|> pure ' '
+sign :: Parser String
+sign = string "+" <|> string "-" <|> string ""
 
-debug = flip trace
+digit :: Parser Char
+digit = match isDigit "Digit"
+
+integer :: Parser String
+integer = Parser $ \inp -> do
+  (pos,  tokens, sign)    <- run sign inp
+  (pos', tokens', digits) <- run (some digit) (pos, tokens)
+  Right (pos', tokens', read . show $ sign ++ digits)
+  
+{--float :: Parser Float
+float = Parser $ \inp -> do
+  (pos,  tokens, sign)       <- run sign inp
+  (pos', tokens', digits)    <- run (some digit) (pos, tokens)
+  (pos'', tokens'', dot)     <- run (string "." <|> string "") (pos', tokens')
+  (pos''', tokens''', float) <- run (many digit) (pos'', tokens'')
+  Right (pos''', tokens''', read . show $ sign ++ digits ++ dot ++ float)-}
